6e7c291e484d1d7c52bd1a81e7108896aff7da5a
Trung Vu <trungvu@trungvu.local>
2018-07-19 20:54:00 +0900

    created container for apache server

---
+++ bin/apache/build_the_blue_apache.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if [ -z "$1" ]
+then
+    echo "==================================================================="
+    echo "| Please input the tag parameter of image!"
+    echo "| latest tag -> is build & pushed by jenkins on default branch."
+    echo "|               you can pull it from https://hub.docker.com/r/chariot9"
+    echo "| uat tag ----> is build & pushed by jenkins on feature branch."
+    echo "==================================================================="
+    exit 1
+fi
+TAG=$1
+docker build --rm -t "the-blue-database:${TAG}" -f dockerfiles/Dockerfile-the-blue-database .
